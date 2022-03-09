58b06972067a3bc18a20fff3f1423e7a8142e435
CodingStorageSoo <chlqudtn347@gmail.com>
2022-03-08 21:32:28 -0800

    3/8

---
+++ 8.ShortestPath/0.11279.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+int n;
+priority_queue<int> pq;
+int main() {
+    cin >> n ;
+    while (n--) {
+        int x ;
+        cin >> x ;
+        if (x == 0) {
+            if (pq.empty()) 
+                cout << "0\n";
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+    return 0;
+}
